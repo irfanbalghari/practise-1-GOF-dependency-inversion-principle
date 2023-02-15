0843329b6fd3f2ca7fd7f844934512a8cba3dbe0
irfanullah <=>
2023-02-15 18:37:46 +0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, Input, SimpleChanges  } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-parent',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class ParentComponent implements OnChanges {
+  @Input() value: any;
+  previousValue: any;
+  currentValue: any;
+  message: any;
   title = 'PersonalTemplate1';
+  ngOnChanges(changes: SimpleChanges) {
+    // console.log('Title changed:', changes['title1'].currentValue);
+
+    if (changes['value']) {
+      this.previousValue = changes['value'].previousValue;
+      this.currentValue = changes['value'].currentValue;
+    }
+  }
 }
+
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <app-parent [value]="inputValue"></app-parent>
+    <button (click)="updateValue()">Update Value</button>
+  `
+})
+export class AppComponent {
+  inputValue = 'initial value';
+
+  updateValue() {
+    this.inputValue = 'updated value';
+  }
+}
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent, ParentComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
